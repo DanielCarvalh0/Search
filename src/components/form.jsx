fb08a44797687893402b4a863d5011eff1412ee9
DanielCarvalh0 <daniel@uuhuw.com>
2023-02-09 14:53:08 -0300

    add Return

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,55 +1,57 @@
 import Pagination from "react-js-pagination"
 
 const Form = () => {
-    <div className="form">
-        <input
-            id="user-name"
-            type="text"
-            placeholder="Digite o username"
-            onChange={(e) => {
-                setIsSubmited(false)
-                setSearch(e.target.value)
-            }}
-            onKeyDown={(e) => enterSubmit(e)}
-            value={search}
-        />
+    return (
+        <div className="form">
+            <input
+                id="user-name"
+                type="text"
+                placeholder="Digite o username"
+                onChange={(e) => {
+                    setIsSubmited(false)
+                    setSearch(e.target.value)
+                }}
+                onKeyDown={(e) => enterSubmit(e)}
+                value={search}
+            />
 
-        {isSubmited && !search ? (
-            <small>Esse campo é obrigatorio</small>
-        ) : null}
+            {isSubmited && !search ? (
+                <small>Esse campo é obrigatorio</small>
+            ) : null}
 
-        <button type="submit" onClick={handleSearch}>
-            Buscar
-        </button>
-        <button type="reset" onClick={reset}>
-            Limpar
-        </button>
+            <button type="submit" onClick={handleSearch}>
+                Buscar
+            </button>
+            <button type="reset" onClick={reset}>
+                Limpar
+            </button>
 
-        {totalResults > 0 ? (
-            <div>
-                <h1 className="search-number">
-                    Resultados encotrados: <br />
-                    {totalResults}
-                </h1>
-            </div>
-        ) : null}
+            {totalResults > 0 ? (
+                <div>
+                    <h1 className="search-number">
+                        Resultados encotrados: <br />
+                        {totalResults}
+                    </h1>
+                </div>
+            ) : null}
 
-        {totalResults > ITEMS_PER_PAGE ? (
-            <div className="page">
-                <ul>
-                    <li>
-                        <Pagination
-                            activePage={activePage}
-                            itemsCountPerPage={ITEMS_PER_PAGE}
-                            totalItemsCount={totalResults}
-                            pageRangeDisplayed={3}
-                            onChange={handlePageChange}
-                        />
-                    </li>
-                </ul>
-            </div>
-        ) : null}
-    </div>
+            {totalResults > ITEMS_PER_PAGE ? (
+                <div className="page">
+                    <ul>
+                        <li>
+                            <Pagination
+                                activePage={activePage}
+                                itemsCountPerPage={ITEMS_PER_PAGE}
+                                totalItemsCount={totalResults}
+                                pageRangeDisplayed={3}
+                                onChange={handlePageChange}
+                            />
+                        </li>
+                    </ul>
+                </div>
+            ) : null}
+        </div>
+    )
 }
 
 export default Form;
--- src/components/result.jsx
+++ src/components/result.jsx
@@ -1,25 +1,27 @@
 const Result = () => {
-    <div className="result">
-        {totalResults > 0 ? (
-            <ul className="list">
-                {results.map((result) => {
-                    return (
-                        <li key={result.id}>
-                            <div>
-                                <img src={result.avatar_url} />
-                                <a
-                                    href={`https://github.com/${result.login}`}
-                                    target="_blank"
-                                >
-                                    <span>{result.login}</span>
-                                </a>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
-        ) : null}
-    </div>
+    return (
+        <div className="result">
+            {totalResults > 0 ? (
+                <ul className="list">
+                    {results.map((result) => {
+                        return (
+                            <li key={result.id}>
+                                <div>
+                                    <img src={result.avatar_url} />
+                                    <a
+                                        href={`https://github.com/${result.login}`}
+                                        target="_blank"
+                                    >
+                                        <span>{result.login}</span>
+                                    </a>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            ) : null}
+        </div>
+    )
 }
 
 export default Result;
